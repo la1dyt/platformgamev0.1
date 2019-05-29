87e217aa430abc37c9339aacccdc247a9c48e69d
Laid <artlear@ayndex.ru>
2019-05-29 21:25:53 +0500

    test

---
+++ Assets/scrips/ads/testads.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Monetization;
+
+public class testads : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Monetization.Initialize("3166054",false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButton("Fire1"))
+        {
+            if (Monetization.IsReady("video"))
+            {
+                ShowAdPlacementContent ad = null;
+                ad = Monetization.GetPlacementContent("video") as ShowAdPlacementContent;
+                if (ad != null)
+                {
+                    ad.Show();
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (Monetization.IsReady("rewardedVideo"))
+            {
+                ShowAdPlacementContent ad = null;
+                ad = Monetization.GetPlacementContent("rewardedVideo") as ShowAdPlacementContent;
+                if (ad != null)
+                {
+                    ad.Show();
+                }
+            }
+        }
+    }
+}
