9cf08410a79f80529d8d2927f8b5a5d9bbe25984
Laid <artlear@ayndex.ru>
2019-05-30 18:37:23 +0500

    menu

---
+++ Assets/scrips/menu/quit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void quitinthegame()
+    {
+        Application.Quit();
+        Debug.Log("Выход работает");
+    }
+}
