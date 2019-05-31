f5e38785dda2be1f2ce9460af29bd24900f0f1f7
Laid <artlear@ayndex.ru>
2019-05-31 12:13:01 +0500

    menu

---
+++ Assets/scrips/menu/buttonclic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class buttonclic : MonoBehaviour
+    
+{
+    public AudioSource clicbutton;
+    public AudioSource music;
+    public quit z;
+    // Start is called before the first frame update
+
+    public void clik()
+    {
+        music.Stop();
+        Debug.Log("-music");
+        clicbutton.Play();
+        Debug.Log("+effect button");
+    }
+}
--- Assets/scrips/menu/quit.cs
+++ Assets/scrips/menu/quit.cs
@@ -1,18 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class quit : MonoBehaviour
+
 {
     // Start is called before the first frame update
+    public int i = 0;
     void Start()
     {
         
     }
 
     // Update is called once per frame
+    public void start()
+    {
+
+    }
+    public void avtor()
+    {
+        Invoke("gotoavtor", 0.5f);
+        
+    }
     public void quitinthegame()
     {
-        Application.Quit();
+        Invoke("quitthegame", 0.5f);
         Debug.Log("Выход работает");
+    }
+    IEnumerator vait(float second)
+    {
+        yield return new WaitForSeconds(second);
+    }
+    public void gotoavtor()
+    {
+        SceneManager.LoadScene("avtor");
+    }
+    public void quitthegame()
+    {
+        Application.Quit();
+    }
+    public void gotothegame()
+    {
+
     }
 }
